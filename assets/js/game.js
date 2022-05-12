5a1367614745f08c3680f1f11cfb3daeafde1a05
lesanndra morton <lesanndra.morton@yahoo.com>
2022-05-12 03:59:21 -0400

    initial commit

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! javascript is running!")
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotat
+ion marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//This will do math and log 20
+console.log(10 + 10);
+
+//What is this?
+console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the robot";
+
+//Tony the robot is ready for battle!
+console.log(playerName + " is ready for battle!")
+
+//"Your robot, Tony the Robot, has won!"
+console.log("Your robot " + playerName + ", has won!")
+
+//this creates a function named "fight"
+
+function fight() {
+    window.alert("This fight has begun!");
+}
+
+// fight();
