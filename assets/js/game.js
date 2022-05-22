bdf1c358ae6901d47425e92582c7cd5f403f6f65
lesanndra morton <lesanndra.morton@yahoo.com>
2022-05-22 15:41:58 -0400

    added Integer input for prompt in shop() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -97,7 +97,7 @@ var startGame = function() {
   if (playerInfo.health > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-    debugger;
+    
 
     // pick new enemy to fight based on the index of the enemyInfo array
     var pickedEnemyObj = enemyInfo[i];
@@ -163,21 +163,18 @@ else {
 var shop = function(){
   //ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
-
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   //use switch to carry out action
   switch (shopOptionPrompt){
-    case "REFILL":
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
     window.alert("Leaving the store.");
       break;
 
@@ -185,7 +182,6 @@ var shop = function(){
     
  default: 
    window.alert("You did not pick a valid option. Try again.");
-
 
    //call shop() again to force player to pick a valid option
    shop();
